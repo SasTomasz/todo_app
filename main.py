ae52edc3621fdd6fcd85ed0d205bd655596e2852
tomasz_sas <tomaszsas.dev@gmail.com>
2023-11-21 08:43:48 +0100

    Remove white space from command user typed, show todos in separate lines

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 user_todos = []
 
 while True:
-    match input("Type add, show or exit: "):
+    match input("Type add, show or exit: ").strip():
         case 'add':
             user_todos.append(input("Enter a todo: "))
         case 'show':
-            print(user_todos)
+            for todo in user_todos:
+                print('*', todo)
         case 'exit':
             break
-        case _ :
+        case _:
             print("Your command did not match, please try again")
 
 print("Bye, bye!")
