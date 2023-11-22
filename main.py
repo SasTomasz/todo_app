739c9635a6d2dd17e83e4fd4085cce6361ed8d84
tomasz_sas <tomaszsas.dev@gmail.com>
2023-11-22 08:58:00 +0100

    Extract showing todos as func, add new feature - editing existing todo

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 user_todos = []
 
+
+def show_todos():
+    for todo in user_todos:
+        print(str(user_todos.index(todo) + 1) + '.', todo)
+
+
 while True:
-    match input("Type add, show or exit: ").strip():
+    match input("Type add, edit, show or exit: ").strip():
         case 'add':
             user_todos.append(input("Enter a todo: "))
         case 'show':
-            for todo in user_todos:
-                print('*', todo)
+            show_todos()
+        case 'edit':
+            print("There are Your todos: ")
+            show_todos()
+            todo_number = int(input("What todo number do You want to edit?: "))
+            new_todo = input("Type new todo description: ")
+            user_todos[todo_number - 1] = new_todo
+            print("Todo was updated \nThis is Your new todos:")
+            show_todos()
         case 'exit':
             break
         case _:
