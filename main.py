8d0f5e56537bcb9cbf1cbac111108a5f02670ac9
Tomasz Sas <tomaszsas.dev@gmail.com>
2023-11-26 15:53:32 +0100

    Save and get todos from a file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-user_todos = []
-
-
 def show_todos():
     if user_todos:
         print("There are Your todos: ")
         for i, todo in enumerate(user_todos):
+            todo = todo.removesuffix('\n')
             print(f"{i + 1}. {todo}")
         return True
     else:
@@ -18,10 +16,31 @@ def is_todo_number_correct(number):
     return number
 
 
+def get_todos_from_file(path='./data/todos.txt'):
+    try:
+        file = open(path, "r")
+        todos = file.readlines()
+        file.close()
+    except FileNotFoundError:
+        file = open(path, "w")
+        file.close()
+        return []
+    return todos
+
+
+def save_todos(todos, path='./data/todos.txt'):
+    file = open(path, "w")
+    file.writelines(todos)
+    file.close()
+
+
+user_todos = get_todos_from_file()
+
 while True:
     match input("Type add, edit, complete, show or exit: ").strip():
         case 'add':
-            user_todos.append(input("Enter a todo: "))
+            user_todos.append(input("Enter a todo: ") + '\n')
+            save_todos(user_todos)
         case 'show':
             show_todos()
         case 'edit':
@@ -29,8 +48,9 @@ while True:
             if user_todos:
                 todo_number = int(input("What todo number do You want to edit?: "))
                 todo_number = is_todo_number_correct(todo_number)
-                new_todo = input("Type new todo description: ")
+                new_todo = input("Type new todo description: ") + '\n'
                 user_todos[todo_number - 1] = new_todo
+                save_todos(user_todos)
                 print("Todo was updated")
                 show_todos()
         case 'complete':
@@ -38,7 +58,9 @@ while True:
             if user_todos:
                 todo_number = int(input("What todo number do You want to complete?: "))
                 todo_number = is_todo_number_correct(todo_number)
-                print(f"TASK {user_todos.pop(todo_number - 1)}, was completed")
+                completed_todo = user_todos.pop(todo_number - 1)
+                save_todos(user_todos)
+                print(f"TASK {completed_todo}, was completed")
                 show_todos()
         case 'exit':
             break
