ebbb2ad78ae2f2178186064e043323e4f92ce687
Tomasz Sas <tomaszsas.dev@gmail.com>
2023-12-26 10:45:10 +0100

    Add list and edit button without implementing it

--- gui.py
+++ gui.py
@@ -6,17 +6,26 @@ from logger import logger
 text_field_desc = sg.Text("Add some todo")
 text_field = sg.InputText(key="todo")
 add_button = sg.Button("Add")
+edit_button = sg.Button("Edit")
+todos = helper_functions.get_todos_from_file()
+list_box = sg.Listbox(todos,
+                      enable_events=True,
+                      key="todos_list",
+                      size=(60, 20))
 
 window = sg.Window("My todo app",
-                   [[text_field_desc, text_field, add_button]])
+                   layout=[[text_field_desc, text_field, add_button],
+                           [list_box, edit_button]])
 while True:
     event, value = window.read()
     logger.info(f"Event was occurred:\nevent: {event}\nvalue: {value['todo']}")
     if event == 'Add':
-        todos = helper_functions.get_todos_from_file()
         todos.append(value['todo'])
         helper_functions.save_todos(todos)
     elif event == sg.WIN_CLOSED:
         break
 
 window.close()
+
+# TODO
+#  * Implement edit button
