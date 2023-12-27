e6e18e12ce0ed565509f318d2d89f593f7949967
tomasz_sas <tomaszsas.dev@gmail.com>
2023-12-27 09:14:25 +0100

    Add Complete button without implementing it yet

--- gui.py
+++ gui.py
@@ -7,15 +7,17 @@ text_field_desc = sg.Text("Add some todo")
 text_field = sg.InputText(key="todo")
 add_button = sg.Button("Add")
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
 todos = helper_functions.get_todos_from_file()
 list_box = sg.Listbox(todos,
                       enable_events=True,
                       key="todos_list",
-                      size=(58, 20))
+                      size=(58, 10))
 
 window = sg.Window("My todo app",
                    layout=[[text_field_desc, text_field, add_button],
-                           [list_box, edit_button]])
+                           [list_box, edit_button],
+                           [complete_button]])
 while True:
     event, value = window.read()
     logger.info(f"Event was occurred:\nevent: {event}\nvalue: {value}")
